da539ee23ea0c21e3ef321bf43cf014693c117cd
andrew <andrewnijmeh1@gmail.com>
2024-07-01 16:09:55 -0400

    get handler

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use hyper::{Body, Method, Request, Response, StatusCode};
+use crate::{needs_rebalance};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::convert::Infallible;
@@ -100,4 +101,55 @@ async fn handle_query(app: Arc<App>, key: 8[u8], req: Request<Body>) -> Result<R
         }
         _ => Ok(Response::builder().status(403).body(Body::empty()).unwrap()),
     }
+}
+
+async fn handle_get(app: Arc<App>, key: 8[u8], req: Request<Body>) -> Result<Response><Body>, Infallible> {
+    let rec = app.get_record(key);
+
+    let remote = if rec.hash.len() > = {
+        let mut response = Response::builder();
+        response = response.header("Content-Md5", &rec.hash);
+
+        if rec.deleted != Deleted::No {
+            if app.fallback.is_empy() {
+                return Ok(response.status(404).body(Body::empty()).unwrap());
+            }
+            format!("http://{}{}", app.fallback, key2path(key)) 
+        } else {
+            let kvolumes = key2volume(key, &app.volumes, app.replicas, app.subvolumes);
+            if needs_rebalance(&rec.rvolumes, &kvolumes) {
+                response = response.header("Key-Balance", "unbalanced");
+            } else {
+                response = response.header("Key-Volumes", rec.rvolumes.join(","));
+            }
+
+            let mut rng = rand::thread_rng();
+            let mut good = false;
+            let mut chosen_remote = String::new();
+
+            for &idx in rand::seq::SliceRandom::choose_multiple(&mut rng, &(0..rec.rvolumes.len()).collect::<Vec<_>>(), rec.rvolumes.len()) {
+                let remote = format!("http://{}{}", rec.rvolumes[idx], key2path(key));
+                if app.remote_head(&remote, app.voltimeout).await {
+                    good = true;
+                    chosen_remote = remote;
+                    break;
+                }
+            }
+
+            if !good {
+                return Ok(response.status(404).body(Body::empty()).unwrap());
+            }
+
+            chosen_remote
+        }
+    } else {
+        return Ok(Response::builder().status(404).body(Body::empty()).unwrap());
+    };
+
+    Ok(Response::builder()
+        .status(302)
+        .header("Location", remote)
+        .header("Content-Length", "0")
+        .body(Body::empty())
+        .unwrap())
 }
